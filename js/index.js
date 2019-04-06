0da498e55a05ff5a3bb31ea5c1c89225628d758b
lazlo barragan <lazlo.barragan@gmail.com>
2019-04-06 23:09:24 +0200

    add animation card desapear

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ var position = "middle";
 var mousePosition = 0;
 var event_choice_1;
 var event_choice_2;
+var reseting = false;
 
 function init() {
     console.log("init");
@@ -16,65 +17,107 @@ function init() {
 }
 
 function onMouseMove(mouseEvent) {
+    if (reseting) {
+        return;
+    }
     var centerX = $(window).width() / 2;
     mousePosition = mouseEvent.clientX - centerX;
     if (mouseEvent.clientX > centerX + mouse_side_threshold) { // right
         console.log("right");
-        anime({
-            targets: '.event-choice-container',
-            translateX: translateXValue,
-            translteY: -translateYValue,
-            rotate: rotateValue,
-            loop: false
-        });
-        event_choice_2.hide();
-        event_choice_1.show();
-        anime({
-            targets: '#event-choice-1',
-            opacity: 1,
-            loop: false
-        });
-        position = "right";
+        slideRight();
+
     } else if (mouseEvent.clientX < centerX - mouse_side_threshold) { // left
         console.log("left");
-        anime({
-            targets: '.event-choice-container',
-            translateX: -translateXValue,
-            translteY: -translateYValue,
-            rotate: -rotateValue,
-            loop: false
-        });
-        event_choice_1.hide();
-        event_choice_2.show();
-        anime({
-            targets: '#event-choice-2',
-            opacity: 1,
-            loop: false
-        });
-        position = "left";
-    } else { //middle
-        anime({
-            targets: '.event-choice-container',
-            translateX: 0,
-            translteY: 0,
-            rotate: '0',
-            loop: false
-        });
-        anime({
-            targets: '#event-choice-1',
-            opacity: 0,
-            loop: false
-        });
-        anime({
-            targets: '#event-choice-2',
-            opacity: 0,
-            loop: false
-        });
+        slideLeft();
+    } else { //middle        
         console.log("middle");
-        position = "middle";
+        resetEvent();
     }
 }
 
 function onMouseClick(mouseEvent) {
-    console.log("click");
+    console.log("click" + position);
+    if (position == "middle") {
+        return;
+    }
+    reseting = true;
+    var i = -1;
+    if (position == "right") {
+        i = 1;
+    }
+    console.log(i);
+    anime({
+        targets: '.event-choice-container',
+        translateX: i * 200,
+        opacity: 0,
+        complete: resetEvent(),
+        loop: false
+    });
 }
+
+function slideLeft() {
+    position = "left";
+    anime({
+        targets: '.event-choice-container',
+        translateX: -translateXValue,
+        translteY: -translateYValue,
+        rotate: -rotateValue,
+        duration: 500,
+        loop: false
+    });
+    event_choice_1.hide();
+    event_choice_2.show();
+    anime({
+        targets: '#event-choice-2',
+        opacity: 1,
+        duration: 500,
+        loop: false
+    });
+}
+
+function slideRight() {
+    position = "right";
+    anime({
+        targets: '.event-choice-container',
+        translateX: translateXValue,
+        translteY: -translateYValue,
+        rotate: rotateValue,
+        duration: 500,
+        loop: false
+    });
+    event_choice_2.hide();
+    event_choice_1.show();
+    anime({
+        targets: '#event-choice-1',
+        opacity: 1,
+        duration: 500,
+        loop: false
+    });
+}
+
+function slideMiddle(callback) {
+    position = "middle";
+    anime({
+        targets: '.event-choice-container',
+        translateX: 0,
+        translteY: 0,
+        rotate: '0',
+        duration: 500,
+        opacity: 1,
+        loop: false
+    });
+    anime({
+        targets: '#event-choice-1',
+        opacity: 0,
+        duration: 500,
+        loop: false
+    });
+    anime({
+        targets: '#event-choice-2',
+        opacity: 0,
+        duration: 500,
+        loop: false
+    });
+}
+
+function resetEvent() {}
